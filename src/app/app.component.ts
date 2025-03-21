ce128e22456095d010cb5efede74ca0b18a65a2f
Erwin Agpasa <rootaccess000@gmail.com>
2025-03-21 09:49:09 +0300

    Flowbite installed (Bug: when file saved flowbite not initialize)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngdashboard';
+
+  constructor(private flowbiteService: FlowbiteService) { }
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+
+
 }
